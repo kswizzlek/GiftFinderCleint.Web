675ddf9822d6ed8f9a36a679b133bc4fb644dfb8
Kevin Kruger <kkruger@foundationsoft.com>
2020-05-22 15:40:36 -0400

    init

---
+++ app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <QuizMain />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import QuizMain from "./components/QuizMain.vue";
+
+@Component({
+  components: {
+    QuizMain
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/QuizMain.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    quiz questions
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
